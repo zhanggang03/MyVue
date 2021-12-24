93e5a4cff4381e259b4ba24cb6f3c3559148ca09
zhanggang2019 <zhanggang2019@boco.com.cn>
2021-12-24 16:10:51 +0800

    补充文档

---
+++ src/components/Search.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <section class="jumbotron">
+            <h3 class="jumbotron-heading"></h3>
+            <div>
+                <input type="text" placeholder="enter the name you search" v-model='keyWord'/> &nbsp;
+                <button @click="getUsers()">Search</button>
+            </div>
+        </section>                
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+   name:'Search',
+   data(){
+       return{
+           keyWord:''
+       }
+   },
+   methods:{
+       getUsers(){
+           //请求前清空List数据
+           this.$bus.$emit('updateListData',{isLoading:true, errMsg:'', isFirst:false});
+           axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
+               reponse => {
+                   console.log('请求成功了');
+                   //请求成功后更新List数据
+                   this.$bus.$emit('updateListData',{isLoading:false,errMsg:'',users:reponse.data.items})
+               },
+               error => {
+                   console.log('请求失败了');
+                   this.$bus.$emit('updateListData',{isLoading:false,errMsg:error.message,users:[]})
+               }
+           )
+       }
+   }
+}
+</script>
+
