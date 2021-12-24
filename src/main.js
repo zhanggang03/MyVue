e9d1f5e78f70ce4cb806fa5cf588e6cc22b946ad
zhanggang2019 <zhanggang2019@boco.com.cn>
2021-12-24 16:09:46 +0800

    补充文件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
+/*
+该文件是整个项目的入口文件
+ */
+
+//引入VUE
 import Vue from 'vue'
+
+//引入APP组件，他是所有组件的父组件
 import App from './App.vue'
 
+//关闭VUE的生产提示
 Vue.config.productionTip = false
 
+//创建VUE的实例对象---VM
 new Vue({
+  //完成功能，将APP组件放入容器中
   render: h => h(App),
+  beforeCreate(){
+    Vue.prototype.$bus = this
+  }
 }).$mount('#app')
